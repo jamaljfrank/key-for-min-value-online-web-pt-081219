8cd5bb5aed03f2222c58c5f50bfefd6e5a633bed
Jamal Frank <jamaljfrank@gmail.com>
2019-09-09 00:09:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -17,3 +17,13 @@ def key_for_min_value(name_hash)
 
   smallest_name
 end
+# lowest_value = nil
+  # lowest_key = nil
+  # name_hash.collect do |name, age|
+    # if lowest_value == nil || age < lowest_value
+      # lowest_value = age
+      # lowest_key = name
+    # end
+  # end
+  # p lowest_key
+# end
